0399774339ada0d6dbf924b1b5ec2b274b2f5129
warrenwebsite <warren.doppia@gmail.com>
2025-12-10 09:02:31 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [react()],
-  base: '/portfolio/', // Une chaîne vide force l'utilisation de chemins relatifs pour les assets
 })
